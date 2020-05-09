860ee21298615379a89f54c76bb6fa916aa37c74
Magnus Markling <magnus@markling.com>
2020-05-09 22:44:06 +0200

    wip

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { AppProps } from 'next/app'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, fas);
+
+import { AppProps } from "next/app";
 
 export default function ({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- src/pages/elever-bootstrap.tsx
+++ src/pages/elever-bootstrap.tsx
@@ -5,7 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
 import ListGroup from "react-bootstrap/ListGroup";
-import Jumbotron from "react-bootstrap/Jumbotron";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default () => {
   return (
@@ -24,63 +24,50 @@ export default () => {
           controlsList="nodownload"
           style={{ width: "100%" }}
         />
-        Tryck här för att gå med >{" "}
-        <Button variant="success">Gå med i Slack</Button>
+        Klicka här för att gå med <br />
+        <Button variant="success">
+          <FontAwesomeIcon icon={["fab", "slack"]} />
+          Gå med i Slack
+        </Button>{" "}
+        <br />
+        <br />
         <Alert variant="danger">Är det fortfarande lite oklart?</Alert>
         Ingen fara, tryck på knappen nedan för att se en guide för hur du kan gå
-        med.
-        <Button variant="success">Ladda ned guide</Button>
+        med. <br />
+        <Button variant="danger">
+          <FontAwesomeIcon icon="book" />
+          Ladda ned guide
+        </Button>
+        <br />
+        <br />
         <Förhållningsregler />
       </main>
     </div>
   );
 };
 
+const FörhållningsreglerItems = [
+  "Vi vill påminna om att enligt appen Slacks användarvillkor är appen inte avsedd för och bör inte användas av någon under 16 år. Det är därför ett krav att du är minst 16 år om du väljer att använda dig av vår hjälpkanaler. Om vårdnadshavare till elever under 16 år vill ställa frågor i våra kanaler är detta OK.",
+  "Be om hjälp i den kanal/channel som passar den fråga du vill ställa. I samband med att du laddar upp frågan, visa eller beskriv hur du hittills försökt lösa uppgiften så blir det lättare för hjälparen att anpassa sin förklaring efter vad just du behöver lära dig.",
+  "Det är viktigt att hjälpen sker i uppgifts-tråden. På så sätt undviker vi att kanalen blir rörig och svårläst.",
+  "Radera inte tråden när du fått den hjälp du behöver – din fråga kan hjälpa andra elever som har svårt för liknande uppgifter.",
+  "Hjälpen hålls i de allmänna kanalerna i en tråd och inte via privata meddelanden. Om någon kontaktar dig i ett privat meddelande, hör av dig till någon av gruppens admins eller moderatorer.",
+  "Syftet med gruppen är att endast få hjälp med skoluppgifter. Om en hjälpare eller annan elev använder gruppen för andra syften eller beter sig på ett olämpligt sätt så leder det till avstängning. Kontakta isåfall genast gruppens admins eller moderatorer.",
+  "Det är inte tillåtet att ställa frågor kring uppgifter där du som elev inte är tillåten att använda yttre hjälp. Det är inte heller tillåtet att fuska. Sekretessbelagda prov, uppgifter eller lösningar får inte heller delas.",
+];
+
 const Förhållningsregler = () => (
-  <Jumbotron>
-    <ListGroup style={{fontSize: '10px'}}>
+  <ListGroup style={{ fontSize: "10px" }}>
+    {FörhållningsreglerItems.map((text) => (
       <ListGroup.Item>
-        Vi vill påminna om att enligt appen Slacks användarvillkor är appen inte
-        avsedd för och bör inte användas av någon under 16 år. Det är därför ett
-        krav att du är minst 16 år om du väljer att använda dig av vår
-        hjälpkanaler. Om vårdnadshavare till elever under 16 år vill ställa
-        frågor i våra kanaler är detta OK.
+        <FontAwesomeIcon icon="check" listItem size="3x" />
+        {text}
       </ListGroup.Item>
-      <ListGroup.Item>
-        Be om hjälp i den kanal/channel som passar den fråga du vill ställa. I
-        samband med att du laddar upp frågan, visa eller beskriv hur du hittills
-        försökt lösa uppgiften så blir det lättare för hjälparen att anpassa sin
-        förklaring efter vad just du behöver lära dig.
-      </ListGroup.Item>
-      <ListGroup.Item>
-        Det är viktigt att hjälpen sker i uppgifts-tråden. På så sätt undviker
-        vi att kanalen blir rörig och svårläst.
-      </ListGroup.Item>
-      <ListGroup.Item>
-        Radera inte tråden när du fått den hjälp du behöver – din fråga kan
-        hjälpa andra elever som har svårt för liknande uppgifter.
-      </ListGroup.Item>
-      <ListGroup.Item>
-        Hjälpen hålls i de allmänna kanalerna i en tråd och inte via privata
-        meddelanden. Om någon kontaktar dig i ett privat meddelande, hör av dig
-        till någon av gruppens admins eller moderatorer.
-      </ListGroup.Item>
-      <ListGroup.Item>
-        Syftet med gruppen är att endast få hjälp med skoluppgifter. Om en
-        hjälpare eller annan elev använder gruppen för andra syften eller beter
-        sig på ett olämpligt sätt så leder det till avstängning. Kontakta
-        isåfall genast gruppens admins eller moderatorer.
-      </ListGroup.Item>
-      <ListGroup.Item>
-        Det är inte tillåtet att ställa frågor kring uppgifter där du som elev
-        inte är tillåten att använda yttre hjälp. Det är inte heller tillåtet
-        att fuska. Sekretessbelagda prov, uppgifter eller lösningar får inte
-        heller delas.
-      </ListGroup.Item>
-    </ListGroup>
-  </Jumbotron>
+    ))}
+  </ListGroup>
 );
 
+//@ts-ignore
 const MenuWithNav = () => (
   <Nav
     className="justify-content-center"
